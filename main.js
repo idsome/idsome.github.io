2d695cd6fc9b3935e459a53e3e5f264151726536
Mohammed Osman <65901587+idsome@users.noreply.github.com>
2022-12-29 00:05:34 +0300

    new changes

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#logo').click(function(){
+        alert("achivement unlocked (features seeker)")
+    })
+    $('#menu').click(function(){
+        $('#ul').toggle();
+    })
+    AOS.init();
+})
